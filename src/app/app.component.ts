be15e0e21681caee165cc7b188d9673f3dab21dd
Guzman Martinez <guzmanmusicoloco@gmail.com>
2020-06-23 10:20:52 +0100

    Guzman
    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-app';
+  title = 'Bienvenido a Angular';
+
+  curso : string = 'Curso Spring';
+  profesor:string ='Guzman';
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+
+import {Cliente} from './cliente';
+
+
+
+export const CLIENTES: Cliente[] = [
+  {id: 1,nombre: 'Guzman', apellido: 'Martinez Santos', email: 'Guzman@email.net', createAt: '2020-06-19'},
+  {id: 2,nombre: 'Marta', apellido: 'Gimenez Muñoz', email: 'Marta@email.net', createAt: '2016-12-29'},
+  {id: 3,nombre: 'Antonio', apellido: 'Muñoz Martinez', email: 'Antonio@email.net', createAt: '2005-06-01'},
+  {id: 4,nombre: 'Jose', apellido: 'Ramirez Muñoz', email: 'Jose@email.net', createAt: '2020-03-27'},
+  {id: 5,nombre: 'Lucia', apellido: 'Peralta Martinez', email: 'Lucia@email.net', createAt: '2014-05-19'},
+  {id: 6,nombre: 'Flor', apellido: 'Tulipan Peralta', email: 'Flor@email.net', createAt: '2000-12-05'},
+]
